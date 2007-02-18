b2e9debb16cd8e99d611865a668e2e2d142731d7
Gauthier Quesnel <quesnel@users.sourceforge.net>
2007-02-18 23:22:32 +0100

    Add license to the unit test programs.

--- src/vle/vpz/test/test2.cpp
+++ src/vle/vpz/test/test2.cpp
@@ -1,3 +1,26 @@
+/** 
+ * @file test2.cpp
+ * @brief Test unit of the vpz parser.
+ * @author The vle Development Team
+ */
+
+/*
+ * Copyright (C) 2007 - The vle Development Team
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
 #include <boost/test/unit_test.hpp>
 #include <boost/test/floating_point_comparison.hpp>
 #include <boost/lexical_cast.hpp>
@@ -12,7 +35,8 @@ void test_vpz()
 {
     const char* xml =
         "<?xml version=\"1.0\"?>\n"
-        "<vpz version=\"0.5\" author=\"Gauthier Quesnel\" date=\"Mon, 12 Feb 2007 23:40:31 +0100\" >\n"
+        "<vpz version=\"0.5\" author=\"Gauthier Quesnel\""
+        " date=\"Mon, 12 Feb 2007 23:40:31 +0100\" >\n"
         " <structures>\n"
         "  <model name=\"test1\" type=\"atomic\">\n"
         "   <init>\n"
