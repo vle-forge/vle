2c356966b4e97ad8cfed405c6b54f5b6af937103
Gauthier Quesnel <quesnel@users.sourceforge.net>
2007-06-15 18:09:56 +0200

    Add observables class.

---
+++ src/vle/vpz/Observables.cpp
@@ -0,0 +1,76 @@
+/** 
+ * @file Observables.cpp
+ * @brief 
+ * @author The vle Development Team
+ * @date ven, 15 jun 2007 17:48:56 +0200
+ */
+
+/*
+ * Copyright (C) 2007 - The vle Development Team
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+#include <vle/vpz/Observables.hpp>
+#include <vle/utils/Tools.hpp>
+#include <vle/utils/Debug.hpp>
+
+namespace vle { namespace vpz {
+
+void Observables::write(std::ostream& out) const
+{
+    if (not empty()) {
+        out << "<observables>\n";
+        for (const_iterator it = begin(); it != end(); ++it) {
+            it->second.write(out);
+        }
+        out << "</observables>\n";
+    }
+}
+
+void Observables::add(const Observables& obs)
+{
+    for (const_iterator it = obs.begin(); it != obs.end(); ++it) {
+        add(it->second);
+    }
+}
+
+Observable& Observables::add(const Observable& obs)
+{
+    Assert(utils::SaxParserError, not exist(obs.name()),
+           (boost::format("Observable %1% already exist") % obs.name()));
+
+    return (*insert(std::make_pair < std::string, Observable >(
+                obs.name(), obs)).first).second;
+}
+
+Observable& Observables::get(const std::string& name)
+{
+    iterator it = find(name);
+    Assert(utils::SaxParserError, it != end(),
+           (boost::format("Observable %1% does not exist") % name));
+
+    return it->second;
+}
+
+const Observable& Observables::get(const std::string& name) const
+{
+    const_iterator it = find(name);
+    Assert(utils::SaxParserError, it != end(),
+           (boost::format("Observable %1% doest not exist") % name));
+
+    return it->second;
+}
+
+}} // namespace vle vpz
---
+++ src/vle/vpz/Observables.hpp
@@ -0,0 +1,62 @@
+/** 
+ * @file Observables.hpp
+ * @brief 
+ * @author The vle Development Team
+ * @date ven, 15 jun 2007 17:46:11 +0200
+ */
+
+/*
+ * Copyright (C) 2007 - The vle Development Team
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+#ifndef VLE_VPZ_OBSERVABLES_HPP
+#define VLE_VPZ_OBSERVABLES_HPP
+
+#include <vle/vpz/Observable.hpp>
+#include <map>
+#include <string>
+
+namespace vle { namespace vpz {
+
+    class Observables : public Base, public std::map < std::string, Observable >
+    {
+    public:
+        Observables()
+        { }
+
+        virtual ~Observables()
+        { }
+
+        virtual void write(std::ostream& out) const;
+
+        virtual Base::type getType() const
+        { return OBSERVABLES; }
+
+        void add(const Observables& obs);
+
+        Observable& add(const Observable& obs);
+
+        Observable& get(const std::string& name);
+
+        const Observable& get(const std::string& name) const;
+
+        inline bool exist(const std::string& name)
+        { return find(name) != end(); }
+    };
+
+}} // namespace vle vpz
+
+#endif
