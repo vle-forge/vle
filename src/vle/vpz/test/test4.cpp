7acec0db9de3340bec2a272192c2cd804db3bde0
Gauthier Quesnel <quesnel@users.sourceforge.net>
2007-07-04 16:59:50 +0200

    Add unit test for the graph::Model::getTargetPortList() function.

---
+++ src/vle/vpz/test/test4.cpp
@@ -0,0 +1,66 @@
+/** 
+ * @file test4.cpp
+ * @brief Test unit of the vpz parser.
+ * @author The vle Development Team
+ */
+
+/*
+ * Copyright (C) 2007 - The vle Development Team
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+#define BOOST_TEST_MAIN
+#define BOOST_AUTO_TEST_MAIN
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE translator_complete_test
+#include <boost/test/unit_test.hpp>
+#include <boost/test/auto_unit_test.hpp>
+#include <boost/test/output_test_stream.hpp>
+#include <vle/vpz/Vpz.hpp>
+#include <vle/vpz/Translator.hpp>
+#include <vle/graph/AtomicModel.hpp>
+#include <vle/graph/CoupledModel.hpp>
+
+using namespace vle;
+
+BOOST_AUTO_TEST_CASE(test_translator)
+{
+    vpz::Vpz vpz;
+    vpz.parse_file("coupled.vpz");
+
+    const vpz::Model& model(vpz.project().model());
+    BOOST_REQUIRE(model.model());
+    BOOST_REQUIRE(model.model()->isCoupled());
+
+    graph::CoupledModel* cpled((graph::CoupledModel*)model.model());
+    BOOST_REQUIRE_EQUAL(cpled->getName(), "top");
+    BOOST_REQUIRE(cpled->exist("top1"));
+    BOOST_REQUIRE(cpled->exist("top2"));
+    BOOST_REQUIRE(cpled->exist("d"));
+    BOOST_REQUIRE(cpled->exist("e"));
+
+    graph::CoupledModel* top1((graph::CoupledModel*)cpled->findModel("top1"));
+    graph::AtomicModel* x((graph::AtomicModel*)top1->findModel("x"));
+
+    graph::TargetModelList out;
+    x->getTargetPortList("out", out);
+
+    BOOST_REQUIRE_EQUAL(out.size(), (graph::TargetModelList::size_type)10);
+
+    //for (graph::TargetModelList::iterator it = out.begin(); it != out.end(); ++it) {
+    //std::cout << "model: " << it->model()->getName() << " port: " <<
+    //it->port() << "\n";
+    //}
+}
