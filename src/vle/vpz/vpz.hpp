62104667fbbbd2f61618f009d3efc1013239e8ac
Gauthier Quesnel <quesnel@users.sourceforge.net>
2007-08-14 13:38:22 +0200

    Add a common header for the VPZ library.

---
+++ src/vle/vpz/vpz.hpp
@@ -0,0 +1,41 @@
+/** 
+ * @file vpz.hpp
+ * @brief 
+ * @author The vle Development Team
+ * @date 2007-08-13
+ */
+
+/*
+ * Copyright (C) 2007 - The vle Development Team
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+#ifndef VLE_VPZ_VPZ_HPP
+#define VLE_VPZ_VPZ_HPP
+
+namespace vle {
+
+    /** 
+     * @brief The namespace vpz is use to parse and to write the VPZ (vle
+     * project file zipped) XML file. vpz uses the Sax parser of the libxml++ to
+     * read VPZ format, Value format and Trame format from a stream.
+     */
+    namespace vpz {
+
+    } // namespace vpz
+
+} // namespace vle
+
+#endif
