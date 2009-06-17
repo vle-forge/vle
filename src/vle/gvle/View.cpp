9dcad93937cced9172bad8f192382245e43dbd33
Guillaume Ansel <anselg@users.sourceforge.net>
2009-06-17 11:58:45 +0200

    gvle: guess file type from name when export graphics
    * It recognizes only the 3 file format supported by the application,
      so: png, pdf or svg only.
      If the user try another one, he gets an error message.
    (closes: #2807254)
    
    Signed-off-by: Patrick Chabrier <patrick.chabrier@toulouse.inra.fr>

--- src/vle/gvle/View.cpp
+++ src/vle/gvle/View.cpp
@@ -241,15 +241,19 @@ void View::exportGraphic()
     file.set_transient_for(*this);
     file.add_button(Gtk::Stock::CANCEL, Gtk::RESPONSE_CANCEL);
     file.add_button(Gtk::Stock::OK, Gtk::RESPONSE_OK);
+    Gtk::FileFilter filterAuto;
     Gtk::FileFilter filterPng;
     Gtk::FileFilter filterPdf;
     Gtk::FileFilter filterSvg;
+    filterAuto.set_name(_("Guess type from file name"));
+    filterAuto.add_pattern("*");
     filterPng.set_name(_("Portable Newtork Graphics (.png)"));
     filterPng.add_pattern("*.png");
     filterPdf.set_name(_("Portable Format Document (.pdf)"));
     filterPdf.add_pattern("*.pdf");
     filterSvg.set_name(_("Scalable Vector Graphics (.svg)"));
     filterSvg.add_pattern("*.svg");
+    file.add_filter(filterAuto);
     file.add_filter(filterPng);
     file.add_filter(filterPdf);
     file.add_filter(filterSvg);
@@ -259,7 +263,22 @@ void View::exportGraphic()
         std::string filename(file.get_filename());
 	std::string extension(file.get_filter()->get_name());
 
-	if (extension == _("Portable Newtork Graphics (.png)"))
+	if (extension == _("Guess type from file name")) {
+	    size_t ext_pos = filename.find_last_of('.');
+	    if (ext_pos != std::string::npos) {
+	        std::string type(filename, ext_pos+1);
+	        filename.resize(ext_pos);
+	        if (type == "png")
+		        mDrawing->exportPng(filename);
+	        else if (type == "pdf")
+		        mDrawing->exportPdf(filename);
+	        else if (type == "svg")
+		        mDrawing->exportSvg(filename);
+		    else
+		        Error(_("Unsupported file format"));
+		}
+	}
+	else if (extension == _("Portable Newtork Graphics (.png)"))
 	    mDrawing->exportPng(filename);
 	else if (extension == _("Portable Format Document (.pdf)"))
 	    mDrawing->exportPdf(filename);
