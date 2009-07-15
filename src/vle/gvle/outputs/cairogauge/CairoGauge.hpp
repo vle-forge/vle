2d3665951bacedce66cdf84e4f7932c8037b8e6b
Guillaume Ansel <anselg@users.sourceforge.net>
2009-07-15 13:52:40 +0200

    gvle: add an Output plugin for CairoGauge.
    * New output plugin 'CairoGauge' with min and max value choices.
    (closes: #2820645)
    
    Signed-off-by: Patrick Chabrier <patrick.chabrier@toulouse.inra.fr>

---
+++ src/vle/gvle/outputs/cairogauge/CairoGauge.hpp
@@ -0,0 +1,55 @@
+/**
+ * @file vle/gvle/outputs/cairogauge/CairoGauge.hpp
+ * @author The VLE Development Team
+ */
+
+/*
+ * VLE Environment - the multimodeling and simulation environment
+ * This file is a part of the VLE environment (http://vle.univ-littoral.fr)
+ * Copyright (C) 2003 - 2008 The VLE Development Team
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef VLE_GVLE_OUTPUTS_CAIROGAUGE_HPP
+#define VLE_GVLE_OUTPUTS_CAIROGAUGE_HPP
+
+#include <vle/gvle/OutputPlugin.hpp>
+#include <gtkmm/filechooserdialog.h>
+#include <gtkmm/dialog.h>
+#include <gtkmm/spinbutton.h>
+
+namespace vle { namespace gvle { namespace outputs {
+
+class CairoGauge : public OutputPlugin
+{
+public:
+    CairoGauge(const std::string& name);
+
+    virtual ~CairoGauge();
+
+    virtual bool start(vpz::Output& output, vpz::View& view);
+
+private:
+    Gtk::Dialog*        m_dialog;
+    Gtk::SpinButton*    m_min;
+    Gtk::SpinButton*    m_max;
+
+    void init(vpz::Output& output);
+    void assign(vpz::Output& output);
+};
+
+}}} // namespace vle gvle outputs
+
+#endif
