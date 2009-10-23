vle-forge/vle

commit 545c782a2e2cc15fd4ef7d9cffad37c6fa316635

gvle: fix bug on graphical navigation in ViewDrawingArea
* When we use double-clic for navigation in coupled model,
  a random segfault appear (closes #2883388).

Signed-off-by: Gauthier Quesnel <quesnel@users.sourceforge.net>

src/vle/gvle/ViewDrawingArea.cpp
@@ -836,11 +836,10 @@ bool ViewDrawingArea::on_button_press_event(GdkEventButton* event)
             on_gvlepointer_button_1(model, shiftOrControl);
             queueRedraw();
         } else if (event->type == GDK_2BUTTON_PRESS and event->button == 1) {
-            mView->showModel(model);
+	    mModeling->addView(model);
         } else if (event->button == 2) {
-            mView->showModel(model);
+	    mModeling->addView(model);
         }
-        queueRedraw();
         break;
     case GVLE::ADDMODEL:
         mView->addAtomicModel(mMouse.get_x(), mMouse.get_y());

commit b748bc2441ccb8f46d578974773f4c50c49aff84

gvle: fix crash when double click on coupled model
A double click on the current showed coupled model crashes GVLE with an
assertion. Bug introduced in 545c782a2e2cc15fd4ef7d9cffad37c6fa316635. Fix by
revert the patch and remove the queue draw function.

src/vle/gvle/Modeling.cpp
@@ -228,17 +228,18 @@ void Modeling::showDynamics(const std::string& /* name */)
 
 void Modeling::addView(graph::Model* model)
 {
-    assert(model);
-    if (model->isCoupled()) {
-        graph::CoupledModel* m = (graph::CoupledModel*)(model);
-        addView(m);
-    } else if (model->isAtomic()) {
-        graph::AtomicModel* graph_am = dynamic_cast<graph::AtomicModel*>(model);
-        try {
-            vpz::AtomicModel& vpz_am = get_model(graph_am);
-            mAtomicBox->show(vpz_am, *graph_am);
-        } catch (utils::SaxParserError& E) {
-            parse_model(mVpz.project().model().atomicModels());
+    if (model) {
+        if (model->isCoupled()) {
+            graph::CoupledModel* m = graph::Model::toCoupled(model);
+            addView(m);
+        } else if (model->isAtomic()) {
+            graph::AtomicModel* m = graph::Model::toAtomic(model);
+            try {
+                vpz::AtomicModel& vm = get_model(m);
+                mAtomicBox->show(vm, *m);
+            } catch (utils::SaxParserError& /*e*/) {
+                parse_model(mVpz.project().model().atomicModels());
+            }
         }
     }
     refreshViews();

src/vle/gvle/ViewDrawingArea.cpp
@@ -836,9 +836,12 @@ bool ViewDrawingArea::on_button_press_event(GdkEventButton* event)
             on_gvlepointer_button_1(model, shiftOrControl);
             queueRedraw();
         } else if (event->type == GDK_2BUTTON_PRESS and event->button == 1) {
-	    mModeling->addView(model);
+	    mView->showModel(model);
         } else if (event->button == 2) {
-	    mModeling->addView(model);
+	    mView->showModel(model);
+        }
+        if (not model) {
+            queueRedraw();
         }
         break;
     case GVLE::ADDMODEL: