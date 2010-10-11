c377439e4da61bcb2dc1346a180ab65dec738207
Gauthier Quesnel <quesnel@users.sourceforge.net>
2010-10-11 14:11:27 +0200

    vle: update headers
    Update all VLE's source code with a new version of headers:
    - copyrights owners
    - authors

--- src/vle/gvle/modeling/difference-equation/Plugin.hpp
+++ src/vle/gvle/modeling/difference-equation/Plugin.hpp
@@ -1,15 +1,15 @@
-/**
- * @file vle/gvle/modeling/difference-equation/Plugin.hpp
- * @author The VLE Development Team
- * See the AUTHORS or Authors.txt file
- */
-
 /*
- * VLE Environment - the multimodeling and simulation environment
- * This file is a part of the VLE environment
+ * @file vle/gvle/modeling/difference-equation/Plugin.hpp
+ *
+ * This file is part of VLE, a framework for multi-modeling, simulation
+ * and analysis of complex dynamical systems
  * http://www.vle-project.org
  *
- * Copyright (C) 2003-2010 ULCO http://www.univ-littoral.fr
+ * Copyright (c) 2003-2007 Gauthier Quesnel <quesnel@users.sourceforge.net>
+ * Copyright (c) 2003-2010 ULCO http://www.univ-littoral.fr
+ * Copyright (c) 2007-2010 INRA http://www.inra.fr
+ *
+ * See the AUTHORS or Authors.txt file for copyright owners and contributors
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
--- src/vle/gvle/modeling/petrinet/TransitionDialog.cpp
+++ src/vle/gvle/modeling/petrinet/TransitionDialog.cpp
@@ -1,15 +1,15 @@
-/**
- * @file vle/gvle/modeling/petrinet/TransitionDialog.cpp
- * @author The VLE Development Team
- * See the AUTHORS or Authors.txt file
- */
-
 /*
- * VLE Environment - the multimodeling and simulation environment
- * This file is a part of the VLE environment
+ * @file vle/gvle/modeling/petrinet/TransitionDialog.cpp
+ *
+ * This file is part of VLE, a framework for multi-modeling, simulation
+ * and analysis of complex dynamical systems
  * http://www.vle-project.org
  *
- * Copyright (C) 2003-2010 ULCO http://www.univ-littoral.fr
+ * Copyright (c) 2003-2007 Gauthier Quesnel <quesnel@users.sourceforge.net>
+ * Copyright (c) 2003-2010 ULCO http://www.univ-littoral.fr
+ * Copyright (c) 2007-2010 INRA http://www.inra.fr
+ *
+ * See the AUTHORS or Authors.txt file for copyright owners and contributors
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -24,6 +24,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 
 #include <vle/gvle/modeling/petrinet/TransitionDialog.hpp>
 #include <vle/utils/i18n.hpp>
