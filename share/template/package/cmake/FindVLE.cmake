636e60eae8b51dce8032367673040dde30508e01
Gauthier Quesnel <quesnel@users.sourceforge.net>
2012-11-05 15:26:32 +0100

    template: fix the find_library in FindVLE.cmake
    The FindVLE.cmake does not use correctly the find_package function of
    CMake. We fix it with correct parameters.

--- share/template/package/cmake/FindVLE.cmake
+++ share/template/package/cmake/FindVLE.cmake
@@ -32,41 +32,47 @@
 #
 
 if (NOT VLE_INCLUDE_DIR)
-	find_path(VLE_INCLUDE_DIR vle/vle.hpp PATHS
-			/usr/include
-			/usr/local/include
-			$ENV{VLE_BASEPATH}/include
-			"[HKEY_LOCAL_MACHINE\\SOFTWARE\\VLE Development Team\\VLE 1.1.0;Path]/include"
-			PATH_SUFFIXES vle-1.1)
+  find_path(VLE_INCLUDE_DIR vle/vle.hpp PATHS
+    /usr/include
+    /usr/local/include
+    $ENV{HOME}/usr/include
+    $ENV{VLE_BASEPATH}/include
+    "[HKEY_LOCAL_MACHINE\\SOFTWARE\\VLE Development Team\\VLE 1.1.0;Path]/include"
+    PATH_SUFFIXES vle-1.1)
 endif ()
 
 if (NOT VLE_LIBRARIES)
-    find_library(VLE_LIBRARIES NAMES vle-1.1
-			/usr/lib
-			/usr/lib64
-			/usr/local/lib
-			/usr/local/lib64
-			$ENV{VLE_BASEPATH}/lib
-			"[HKEY_LOCAL_MACHINE\\SOFTWARE\\VLE Development Team\\VLE 1.1.0\\;Path]/lib"
-			PATH_SUFFIXES lib)
+  find_library(VLE_LIBRARIES
+    NAMES vle-1.1 libvle-1.1
+    HINTS $ENV{HOME}/usr $ENV{VLE_BASEPATH} 
+    PATH_SUFFIXES lib lib64
+    PATHS 
+    /usr
+    /usr/local 
+    "[HKEY_LOCAL_MACHINE\\SOFTWARE\\VLE Development Team\\VLE 1.1.0\\;Path]/lib")
 endif ()
 
 if (NOT VLE_STATIC_LIBRARIES)
-	find_library(VLE_STATIC_LIBRARIES NAMES vle-1.1.a
-			/usr/lib
-			/usr/lib64
-			/usr/local/lib
-			/usr/local/lib64
-			$ENV{VLE_BASEPATH}/lib
-			"[HKEY_LOCAL_MACHINE\\SOFTWARE\\VLE Development Team\\VLE 1.1.0\\;Path]/lib"
-			PATH_SUFFIXES lib)
+  find_library(VLE_STATIC_LIBRARIES
+    NAMES vle-1.1.a libvle-1.1.a
+    HINTS $ENV{HOME}/usr $ENV{VLE_BASEPATH} 
+    PATH_SUFFIXES lib lib64
+    PATHS 
+    /usr
+    /usr/local 
+    "[HKEY_LOCAL_MACHINE\\SOFTWARE\\VLE Development Team\\VLE 1.1.0\\;Path]/lib")
+endif ()
+
+if (NOT VLE_STATIC_LIBRARIES OR NOT VLE_LIBRARIES OR NOT VLE_INCLUDE_DIR)
+  message(STATUS "VLE include directories: ${VLE_INCLUDE_DIR}")
+  message(STATUS "VLE libraries: ${VLE_LIBRARIES}")
+  message(STATUS "VLE static libraries: ${VLE_STATIC_LIBRARIES}")
 endif ()
 
 # handle the QUIETLY and REQUIRED arguments and set VLE_FOUND to TRUE if
 # all listed variables are TRUE
 include(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(VLE DEFAULT_MSG
-								  REQUIRED_VARS VLE_LIBRARIES
-								  VLE_STATIC_LIBRARIES VLE_INCLUDE_DIR)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(VLE REQUIRED_VARS VLE_LIBRARIES
+  VLE_STATIC_LIBRARIES VLE_INCLUDE_DIR)
 
 mark_as_advanced(VLE_INCLUDE_DIR VLE_LIBRARIES VLE_STATIC_LIBRARIES)
