fbd841deeb0434fbddf05bbe791a16f2a0b6af82
Ronan Trépos <ronan.trepos@toulouse.inra.fr>
2016-09-01 17:23:53 +0200

    gvle.default: add a condition plugin
    a first plugin is provided to initalize dates.

---
+++ src/pkgs/gvle.default/DateCondition/DateCondition.cpp
@@ -0,0 +1,138 @@
+/*
+ * This file is part of VLE, a framework for multi-modeling, simulation
+ * and analysis of complex dynamical systems
+ * http://www.vle-project.org
+ *
+ * Copyright (c) 2015 INRA http://www.inra.fr
+ *
+ * See the AUTHORS or Authors.txt file for copyright owners and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <QObject>
+#include <QtPlugin>
+#include "DateCondition.h"
+#include <iostream>
+#include <vle/utils/DateTime.hpp>
+#include <vle/vpz/AtomicModel.hpp>
+#include <vle/value/String.hpp>
+#include <vle/value/Integer.hpp>
+
+namespace vle{
+namespace gvle{
+
+/**
+ * @brief DateCondition::DateCondition
+ *        Default constructor
+ */
+DateCondition::DateCondition() :
+  mSettings(0), mLogger(0),
+  mExpCond(), mVpm(0), mcalendar(0)
+
+{
+    mcalendar = new QCalendarWidget();
+    QObject::connect(mcalendar, SIGNAL(destroyed(QObject*)),
+                         this,         SLOT(calendarDestroyed(QObject*)) );
+}
+
+DateCondition::~DateCondition()
+{
+    mcalendar->deleteLater();
+}
+
+QString
+DateCondition::getname()
+{
+    QString name = "date";
+    return name;
+}
+
+
+void
+DateCondition::setSettings(QSettings *s)
+{
+    mSettings = s;
+}
+
+void
+DateCondition::setLogger(Logger *logger)
+{
+    mLogger = logger;
+}
+
+QWidget*
+DateCondition::getWidget()
+{
+
+    return mcalendar;
+}
+
+void
+DateCondition::init(vleVpm* vpm, const QString& cond)
+{
+    // Save it
+    mVpm = vpm;
+    mExpCond = cond;
+
+    QDomNode datePort;
+
+    // Search the "date" port of the experimental condition
+
+    QDomNode condXml = mVpm->condFromConds(mVpm->condsFromDoc(), cond);
+
+    if (not mVpm->existPortFromCond(condXml, "date")) {
+        value::Map portValues;
+        portValues.addInt("date",utils::DateTime::toJulianDayNumber(
+                "2016-09-01"));
+        vleVpz::fillConditionWithMap(mVpm->getDomDoc(),condXml,portValues);
+    }
+
+    std::unique_ptr<value::Value> dateV = std::move(
+            mVpm->buildValueFromDoc(cond, "date",0));
+    double dateI = dateV->toInteger().value();
+    int dayI = utils::DateTime::dayOfMonth(dateI);
+    int monthI = utils::DateTime::month(dateI);
+    int yearI = utils::DateTime::year(dateI);
+
+    QDate selDate;
+    selDate.setDate(yearI, monthI, dayI);
+
+    QObject::disconnect(mcalendar, SIGNAL(clicked(QDate)),
+                     this,         SLOT(dateSelected(QDate)) );
+    mcalendar->setSelectedDate(selDate);
+    mcalendar->setCurrentPage(monthI, yearI);
+    mcalendar->showSelectedDate();
+    QObject::connect(mcalendar, SIGNAL(clicked(QDate)),
+                     this,         SLOT(dateSelected(QDate)) );
+}
+
+void
+DateCondition::calendarDestroyed(QObject *obj)
+{
+    (void) obj;
+}
+
+void
+DateCondition::dateSelected(QDate date)
+{
+    value::Map portValues;
+    portValues.addInt("date",utils::DateTime::toJulianDayNumber(
+            date.toString("yyyy-MM-dd").toStdString()));
+    mVpm->fillConditionWithMapToDoc(mExpCond,portValues);
+    mcalendar->setCurrentPage(date.month(), date.year());
+    mcalendar->showSelectedDate();
+}
+}}//namespaces
+
---
+++ src/pkgs/gvle.default/DateCondition/DateCondition.h
@@ -0,0 +1,77 @@
+/*
+ * This file is part of VLE, a framework for multi-modeling, simulation
+ * and analysis of complex dynamical systems
+ * http://www.vle-project.org
+ *
+ * Copyright (c) 2015 INRA http://www.inra.fr
+ *
+ * See the AUTHORS or Authors.txt file for copyright owners and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef GVLE_CONDDATEPLUGIN_H
+#define GVLE_CONDDATEPLUGIN_H
+
+#include <QObject>
+#include <QSettings>
+#include <QDate>
+#include <QtWidgets/QCalendarWidget>
+#include <vle/utils/Package.hpp>
+#include <vle/gvle/plugin_cond.h>
+#include <vle/gvle/logger.h>
+#include <vle/gvle/vlevpm.h>
+
+//#include "tab.h"
+//#include "toolbar.h"
+
+namespace vle{
+namespace gvle{
+
+
+
+class DateCondition : public PluginExpCond
+{
+    Q_OBJECT
+    Q_PLUGIN_METADATA(IID "fr.inra.vle.gvle.PluginExpCond")
+    Q_INTERFACES(vle::gvle::PluginExpCond)
+
+public:
+    DateCondition();
+    ~DateCondition();
+    QString getname() override;
+    QWidget* getWidget() override;
+    void setSettings(QSettings *s) override;
+    void setLogger(Logger *logger) override;
+    void init(vleVpm*vpm, const QString& cond) override;
+    PluginExpCond* newInstance() {return new DateCondition();}
+
+public slots:
+    void dateSelected(QDate date);
+    void calendarDestroyed(QObject *obj);
+
+private:
+    QSettings*  mSettings;
+    Logger*     mLogger;
+    //MainTab*    mWidgetTab;
+    //widToolbar* mWidgetToolbar;
+
+    QString     mExpCond;
+    vleVpm*     mVpm;
+    QCalendarWidget* mcalendar;
+};
+
+}}//namespaces
+
+#endif // EXPCOND_DUMMY_H
