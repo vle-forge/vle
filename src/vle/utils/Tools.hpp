f9b928bde82a1cec84c90d23ca71768e4c076bb1
Gauthier Quesnel <gauthier.quesnel@toulouse.inra.fr>
2016-11-02 14:26:55 +0100

    utils: adds a numeric_cast<T>(S) constexpr function
    This function ensures that a cast between integer are valid (digits and
    sign) before cast with static_cast<T>(S).
    
    We use a VLE_CXX14_CONSTEXPR macro to enable constexpr on GCC >= 5 and
    clang with cxx_relaxed_constexpr feature definition.

--- src/vle/utils/Tools.hpp
+++ src/vle/utils/Tools.hpp
@@ -24,165 +24,261 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-
 #ifndef VLE_UTILS_TOOLS_HPP
 #define VLE_UTILS_TOOLS_HPP
 
 #include <vle/DllDefines.hpp>
 #include <typeinfo>
 #include <string>
+#include <limits>
 #include <vector>
+#include <stdexcept>
 
 #if defined(__GNUC__)
-#define GCC_ATTRIBUTE_FORMAT(idformat, idarg)                   \
+#define GCC_ATTRIBUTE_FORMAT(idformat, idarg)                                 \
     __attribute__((format(printf, (idformat), (idarg))))
 #else
 #define GCC_ATTRIBUTE_FORMAT(idformat, idarg)
 #endif
 
-namespace vle { namespace utils {
+#if defined(__GNUC__) && !defined(__clang__)
+#if __GNUC__ >= 5
+#define VLE_CXX11_CONSTEXPR constexpr
+#define VLE_CXX14_CONSTEXPR constexpr
+#else
+#define VLE_CXX11_CONSTEXPR constexpr
+#define VLE_CXX14_CONSTEXPR
+#endif
+#elif defined(__clang__)
+#if __has_feature(cxx_relaxed_constexpr)
+#define VLE_CXX11_CONSTEXPR constexpr
+#define VLE_CXX14_CONSTEXPR constexpr
+#else
+#define VLE_CXX11_CONSTEXPR constexpr
+#define VLE_CXX14_CONSTEXPR
+#endif
+#else
+#define VLE_CXX11_CONSTEXPR
+#define VLE_CXX14_CONSTEXPR
+#endif
 
-    /**
-     * The function \c format() produce a \c std::string according to a C
-     * printf format as described in standard \c printf() function.
-     *
-     * \code
-     * std::string A = vle::utils::format("%d is good!", 1);
-     * std::string B = vle::utils::format("%s %s!", "hello", "world");
-     * \endcode
-     *
-     * \param format The format to convert.
-     *
-     * \return a empty string if an error occured otherwise the \e printf()
-     * formatted string.
-     */
-    std::string VLE_API
-    format(const char *format, ...) noexcept GCC_ATTRIBUTE_FORMAT(1, 2);
+namespace vle
+{
+namespace utils
+{
 
-    /**
-     * The function \c vformat() produces a \c std::string according to a C
-     * printf format as described in standard \c vprintf() function.
-     *
-     * \param format The format to convert.
-     * \param ap Arguments.
-     *
-     * \return a empty string if an error occured otherwise the \e printf()
-     * formatted string.
-     */
-    std::string VLE_API
-    vformat(const char *format, va_list ap) noexcept;
+/**
+ * The function \c format() produce a \c std::string according to a C
+ * printf format as described in standard \c printf() function.
+ *
+ * \code
+ * std::string A = vle::utils::format("%d is good!", 1);
+ * std::string B = vle::utils::format("%s %s!", "hello", "world");
+ * \endcode
+ *
+ * \param format The format to convert.
+ *
+ * \return a empty string if an error occured otherwise the \e printf()
+ * formatted string.
+ */
+std::string VLE_API
+format(const char *format, ...) noexcept GCC_ATTRIBUTE_FORMAT(1, 2);
 
-    /**
-     * Return true if the string \c str can be translated the template type \c T.
-     * The template type \c T can be one of the following type: bool, float,
-     * double, int8_t, uint8_t, int16_t, uint16_t, int32_t or uint32_t.
-     *
-     * @param str The string to convert.
-     *
-     * @return true if string can be translated into the type \c T.
-     */
-    template < typename T > bool is(const std::string& str);
+/**
+ * The function \c vformat() produces a \c std::string according to a C
+ * printf format as described in standard \c vprintf() function.
+ *
+ * \param format The format to convert.
+ * \param ap Arguments.
+ *
+ * \return a empty string if an error occured otherwise the \e printf()
+ * formatted string.
+ */
+std::string VLE_API vformat(const char *format, va_list ap) noexcept;
 
-    /**
-     * Convert the template type \c T into the string representation.
-     * The template type \c T can be one of the following type: bool, float,
-     * double, int8_t, uint8_t, int16_t, uint16_t, int32_t or uint32_t.
-     *
-     * @param t An instance of the type \c T to convert.
-     *
-     * @return A string representation.
-     */
-    template < typename T > std::string to(const T t);
+/**
+ * Return true if the string \c str can be translated the template type \c T.
+ * The template type \c T can be one of the following type: bool, float,
+ * double, int8_t, uint8_t, int16_t, uint16_t, int32_t or uint32_t.
+ *
+ * @param str The string to convert.
+ *
+ * @return true if string can be translated into the type \c T.
+ */
+template <typename T> bool is(const std::string &str);
 
-    /**
-     * Convert the string \c str into the template type \c T. The template type
-     * \c T can be one of the following type: bool, float, double, int8_t,
-     * uint8_t, int16_t, uint16_t, int32_t or uint32_t.
-     *
-     * @param str The string to convert.
-     *
-     * @throw utils::ArgError If the string \c str can not be converted.
-     *
-     * @return A instance of the type \c T.
-     */
-    template < typename T > T to(const std::string& str);
+/**
+ * Convert the template type \c T into the string representation.
+ * The template type \c T can be one of the following type: bool, float,
+ * double, int8_t, uint8_t, int16_t, uint16_t, int32_t or uint32_t.
+ *
+ * @param t An instance of the type \c T to convert.
+ *
+ * @return A string representation.
+ */
+template <typename T> std::string to(const T t);
 
-    /**
-     * Convert the string to the output template type with or without
-     * the use of locale.
-     * \c T can be one of the following type: bool, float, double, int8_t,
-     * uint8_t, int16_t, uint16_t, int32_t or uint32_t.
-     * @code
-     * vle::utils::convert < double >("123.456");
-     * // returns 123.456
-     *
-     * vle::utils::convert < double >("123 456,789", true, "fr_FR");
-     * // returns 123456.789
-     * @endcode
-     *
-     * @param value The string to convert
-     * @param locale True to use the default locale, false otherwise.
-     * @param loc The locale to use.
-     *
-     * @throw utils::ArgError if a convertion failed.
-     *
-     * @return A template output type.
-     */
-    template < typename T > T convert(const std::string& value,
-                                      bool locale = false,
-                                      const std::string& loc = std::string());
+/**
+ * Convert the string \c str into the template type \c T. The template type
+ * \c T can be one of the following type: bool, float, double, int8_t,
+ * uint8_t, int16_t, uint16_t, int32_t or uint32_t.
+ *
+ * @param str The string to convert.
+ *
+ * @throw utils::ArgError If the string \c str can not be converted.
+ *
+ * @return A instance of the type \c T.
+ */
+template <typename T> T to(const std::string &str);
 
-    /**
-     * Check if the locale exists on this operating system.
-     * @param locale The locale to check.
-     * @return True if the locale exists, false otherwise.
-     */
-    VLE_API bool isLocaleAvailable(const std::string& locale);
+/**
+ * Convert the string to the output template type with or without
+ * the use of locale.
+ * \c T can be one of the following type: bool, float, double, int8_t,
+ * uint8_t, int16_t, uint16_t, int32_t or uint32_t.
+ * @code
+ * vle::utils::convert < double >("123.456");
+ * // returns 123.456
+ *
+ * vle::utils::convert < double >("123 456,789", true, "fr_FR");
+ * // returns 123456.789
+ * @endcode
+ *
+ * @param value The string to convert
+ * @param locale True to use the default locale, false otherwise.
+ * @param loc The locale to use.
+ *
+ * @throw utils::ArgError if a convertion failed.
+ *
+ * @return A template output type.
+ */
+template <typename T>
+T convert(const std::string &value,
+          bool locale = false,
+          const std::string &loc = std::string());
 
-    /**
-     * Return the a string version of v, in scientific notation
-     * (if v is too small or big) or in a standard representaion.
-     *
-     * @param v double to convert.
-     * @param locale  if true, use "," else use "."
-     * @return the string representation of the double.
-     */
-    VLE_API std::string toScientificString(
-        const double& v, bool locale = false);
+/**
+ * Check if the locale exists on this operating system.
+ * @param locale The locale to check.
+ * @return True if the locale exists, false otherwise.
+ */
+VLE_API bool isLocaleAvailable(const std::string &locale);
 
-    /**
-     * Demangle the input type info from C++ compiler.
-     * http://gcc.gnu.org/onlinedocs/libstdc++/latest-doxygen/namespaceabi.html
-     *
-     * @param in the std::type_info to demangle.
-     *
-     * @return the demangled string or the same if libcwd is not linked.
-     */
-    VLE_API std::string demangle(const std::type_info& in);
+/**
+ * Return the a string version of v, in scientific notation
+ * (if v is too small or big) or in a standard representaion.
+ *
+ * @param v double to convert.
+ * @param locale  if true, use "," else use "."
+ * @return the string representation of the double.
+ */
+VLE_API std::string toScientificString(const double &v, bool locale = false);
 
-    /**
-     * Demangle the input string from C++ compiler.
-     * http://gcc.gnu.org/onlinedocs/libstdc++/latest-doxygen/namespaceabi.html
-     *
-     * @param in the string to demangle.
-     *
-     * @return the demangled string or the same if libcwd is not linked.
-     */
-    VLE_API std::string demangle(const std::string& in);
+/**
+ * Demangle the input type info from C++ compiler.
+ * http://gcc.gnu.org/onlinedocs/libstdc++/latest-doxygen/namespaceabi.html
+ *
+ * @param in the std::type_info to demangle.
+ *
+ * @return the demangled string or the same if libcwd is not linked.
+ */
+VLE_API std::string demangle(const std::type_info &in);
 
-    /**
-     * Tokenize a string with a delimiter
-     * @param[in]  str, the string to tokenize
-     * @param[out] toks, vector filled with tokens
-     * @param[in]  delim, the delimiter for tokenizing
-     * @param[in]  trimEmpty, if true remove empty tokens
-     * @return the tokens
-     */
-    VLE_API  void tokenize(const std::string& str,
-            std::vector<std::string>& toks, const std::string& delim,
-            bool trimEmpty);
+/**
+ * Demangle the input string from C++ compiler.
+ * http://gcc.gnu.org/onlinedocs/libstdc++/latest-doxygen/namespaceabi.html
+ *
+ * @param in the string to demangle.
+ *
+ * @return the demangled string or the same if libcwd is not linked.
+ */
+VLE_API std::string demangle(const std::string &in);
 
+/**
+ * Tokenize a string with a delimiter
+ * @param[in]  str, the string to tokenize
+ * @param[out] toks, vector filled with tokens
+ * @param[in]  delim, the delimiter for tokenizing
+ * @param[in]  trimEmpty, if true remove empty tokens
+ * @return the tokens
+ */
+VLE_API void tokenize(const std::string &str,
+                      std::vector<std::string> &toks,
+                      const std::string &delim,
+                      bool trimEmpty);
 
-}} // namespace vle utils
+/**
+ * @brief Return true if @c Source can be casted into @c Target integer
+ * type.
+ * @details Check if the integer type @c Source is castable into @c Target.
+ *
+ * @param arg A value.
+ * @tparam Source An integer type.
+ * @tparam Target An integer type.
+ * @return true if @c static_cast<Target>(Source) is valid.
+ */
+template <typename Target, typename Source>
+VLE_CXX14_CONSTEXPR inline bool is_numeric_castable(Source arg)
+{
+    static_assert(std::is_integral<Source>::value, "Integer required.");
+    static_assert(std::is_integral<Target>::value, "Integer required.");
+
+    using arg_traits = std::numeric_limits<Source>;
+    using result_traits = std::numeric_limits<Target>;
+
+    if (result_traits::digits == arg_traits::digits and
+        result_traits::is_signed == arg_traits::is_signed)
+        return true;
+
+    if (result_traits::digits > arg_traits::digits)
+        return result_traits::is_signed or arg >= 0;
+
+    if (arg_traits::is_signed and
+        arg < static_cast<Source>(result_traits::min()))
+        return false;
+
+    return arg <= static_cast<Source>(result_traits::max());
+}
+
+/**
+ * @brief An internal exception when an integer cast fail.
+ * @details @c numeric_cast_error is used with the
+ * @e vle::utils::numerci_cast<TargetT>(SourceT) function that cast
+ * integer type to another.
+ */
+struct VLE_API numeric_cast_error : public std::exception {
+    virtual const char *what() const noexcept
+    {
+        return "numeric cast error: loss of range in numeric_cast";
+    }
+};
+
+/**
+ * @brief Tries to convert @c Source into @c Target integer.
+ * @code
+ * std::vector<int> v(1000, 5);
+ *
+ * // No exception.
+ * int i = vle::utils::numeric_cast<int>(v.size());
+ *
+ * // Throw exception.
+ * int8_t j = vle::utils::numeric_cast<int8_t>(v.size());
+ * @endcode
+ *
+ * @param arg A value.
+ * @tparam Source An integer type.
+ * @tparam Target An integer type.
+ * @return @c static_cast<Target>(Source) integer.
+ */
+template <typename Target, typename Source>
+VLE_CXX14_CONSTEXPR inline Target numeric_cast(Source s)
+{
+    if (not is_numeric_castable<Target>(s))
+        throw vle::utils::numeric_cast_error();
+
+    return static_cast<Target>(s);
+}
+}
+} // namespace vle utils
 
 #endif
