715d8b5a4377214d3e26995f4cd53c47550f43d5
Gauthier Quesnel <gauthier.quesnel@inra.fr>
2017-11-13 16:34:08 +0100

    template: update default cpp file
    * add documentation and how to for each function
    * remove virtual since override is used
    * remove unnecessary namespace affectation

--- share/template/package/src/Simple.cpp
+++ share/template/package/src/Simple.cpp
@@ -31,70 +31,119 @@
  * @@tagdepends:@@endtagdepends
  */
 
-#include <vle/value/Value.hpp>
 #include <vle/devs/Dynamics.hpp>
+#include <vle/value/Value.hpp>
 
-namespace vd = vle::devs;
-namespace vv = vle::value;
+namespace example {
 
-namespace examples {
-
-class Simple : public vd::Dynamics
+class Simple : public vle::devs::Dynamics
 {
 public:
-    Simple(const vd::DynamicsInit& init, const vd::InitEventList& events)
-        : vd::Dynamics(init, events)
+    Simple(const vle::devs::DynamicsInit& init,
+           const vle::devs::InitEventList& events)
+      : vle::devs::Dynamics(init, events)
     {
     }
 
-    virtual ~Simple()
+    ~Simple() override = default;
+
+    //
+    // Process the initialization of the model by initializing the initial
+    // state and return the duration of the initial state.
+    // Default, @c init function returns vle::devs::infinity
+    //
+    vle::devs::Time init(vle::devs::Time time) override
     {
+        return vle::devs::Dynamics::init(time);
     }
 
-    virtual vd::Time init(vle::devs::Time /*time*/) override
+    //
+    // Process the output function: add external event into the @c output.
+    //
+    void output(vle::devs::Time time,
+                vle::devs::ExternalEventList& output) const override
     {
-        return vd::infinity;
+        // Example:
+        // output.emplace_back("out");
+        // output.back().addDouble(0.001);
+
+        vle::devs::Dynamics::output(time, output);
     }
 
-    virtual void output(vle::devs::Time /*time*/,
-                        vd::ExternalEventList& /*output*/) const override
+    //
+    // Compute the duration of the current state. As the @c init function, @c
+    // timeAdvance function default returns vle::devs::infinity.
+    //
+    vle::devs::Time timeAdvance() const override
     {
+        return vle::devs::Dynamics::timeAdvance();
     }
 
-    virtual vd::Time timeAdvance() const override
+    //
+    // Compute the new state of the model with the internal transition
+    // function. Default function does nothing.
+    //
+    void internalTransition(vle::devs::Time time) override
     {
-        return vd::infinity;
+        vle::devs::Dynamics::internalTransition(time);
     }
 
-    virtual void internalTransition(vle::devs::Time /*time*/) override
+    //
+    // Compute the new state of the model when at least one external event
+    // occurs. Default function does nothing.
+    //
+    void externalTransition(const vle::devs::ExternalEventList& events,
+                            vle::devs::Time time) override
     {
-    }
+        // Example:
+        // for (const auto& elem : events) {
+        //     if (elem.attributes()->isDouble()) {
+        //         //         double value = elem.getDouble().value();
+        //     }
+        // }
 
-    virtual void externalTransition(
-            const vd::ExternalEventList& /*event*/,
-            vle::devs::Time /*time*/) override
-    {
-    }
-
-    virtual void confluentTransitions(
-            vle::devs::Time time,
-            const vd::ExternalEventList& events) override
-    {
-        internalTransition(time);
         externalTransition(events, time);
     }
 
-    virtual std::unique_ptr<vle::value::Value> observation(
-            const vd::ObservationEvent& /*event*/) const override
+    //
+    // Compute the new state of the model when both at least one external event
+    // occurs and an internal event. Default function is to call internal
+    // transition and external transition as follow:
+    //
+    void confluentTransitions(
+      vle::devs::Time time,
+      const vle::devs::ExternalEventList& events) override
     {
-        return 0;
+        // Example of code:
+        // internalTransition(time);
+        // externalTransition(events, time);
+
+        confluentTransitions(time, events);
     }
 
-    virtual void finish()
+    //
+    // Compute the current state of the model at a specified time and for a
+    // specified observation port.
+    //
+    std::unique_ptr<vle::value::Value> observation(
+      const vle::devs::ObservationEvent& event) const override
     {
+        // Example:
+        // return vle::Double::create(123.321);
+
+        return vle::devs::Dynamics::observation(event);
+    }
+
+    //
+    // When the simulation of the atomic model is finished, the finish method
+    // is invoked. Default function does nothing.
+    //
+    void finish()
+    {
+        vle::devs::Dynamics::finish();
     }
 };
 
-} // namespace vle example
+} // namespace example
 
-DECLARE_DYNAMICS(examples::Simple)
+DECLARE_DYNAMICS(example::Simple)
